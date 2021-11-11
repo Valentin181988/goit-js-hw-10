93c89dce1f3e7a6c305c50910190adeffd47daae
Valentin Sachenko <valentin181988@gmail.com>
2021-11-11 18:20:40 +0300

    last

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const debounce = require('lodash.debounce');
-
 const input = document.querySelector("#search-box");
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
-input.addEventListener('input', () => {
+input.addEventListener('input', debounce(() => {
     resetCountries();
+    console.log("deb")
 
     const name = input.value;
 
@@ -16,7 +16,7 @@ input.addEventListener('input', () => {
     }
 
     fetchCountries(name).then(responceProcesing);
-});
+}, 300));
 
 function fetchCountries(name) {
     return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,currencies,flags,languages`)
@@ -40,17 +40,34 @@ function responceProcesing(countries) {
         countries.forEach((country) => {
             countryRender(country);
         });
-    } 
+    };
+
+    if (countries.length === 1) {
+        countries.forEach((country) => {
+            countryCardRender(country);
+        });    
+    };
 }
 
 function countryRender(country) {
     countryList
-        .insertAdjacentHTML("beforeend", `<li class ="list"><img src="${country.flags.svg}" class ="size list">${country.name.official}</li>`);
-    
+        .insertAdjacentHTML("beforeend", `<li class ="list"><img src="${country.flags.svg}" class ="size">${country.name.official}</li>`);   
+}
+
+function countryCardRender(country) {
+    countryInfo.insertAdjacentHTML("beforeend",
+        `<ul>
+           <li class ="list">${country.name.official}</li>
+           <li class ="list">${country.capital}</li>
+           <li class ="list">${country.population}</li>
+           <li class ="list" <img src="${country.flags.svg}" class ="size"></li>
+           <li class ="list">${country.languages}</li>
+        </ul>`);
 }
 
 function resetCountries() {
     countryList.innerHTML = "";
+    countryInfo.innerHTML = "";
 }
 
 
