4de6393a69844e575be5ae20f5000484c9cf37a0
Valentin Sachenko <valentin181988@gmail.com>
2021-11-12 17:58:22 +0300

    last

--- src/fetch-api.js
+++ src/fetch-api.js
@@ -1,3 +1,4 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 function fetchCountries(name) {
     return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
